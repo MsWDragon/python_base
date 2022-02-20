5455fbf46aa22f11410929a2b20141205e85e042
MsWFoX <dragon.nest9821@gmail.com>
2022-02-20 18:05:52 +0600

    complete task 1.2.3

---
+++ Norovyatkin_Ilya_dz_9/task_9_2.py
@@ -0,0 +1,27 @@
+from turtle import window_height
+
+
+class Road:
+    
+    def __init__(self, length: int, width: int):
+        """конструктор класса
+        :param length: длинна в метрах
+        :param width: ширина в метрах
+        """
+        # опишите конструктор в соответствии с заданием
+        self._length = length
+        self._width = width
+
+    def calculate(self, hight: int = 5, mass_m_2: int = 25) -> int:
+        """
+        считает масу массу асфальта, необходимого для покрытия всей дороги в тоннах
+        :param hight: высота дорожного полотна в сантиметрах
+        :param mass_m_2: масса в кг квадратного метра дороги высотой 1 см
+        :return: int значение тонн, дробная часть если есть НЕ учитывается
+        """
+        # Ваш код для рассчёта массы в тоннах
+        total_mass = self._length * self._width * mass_m_2 * hight
+        return total_mass // 1000
+if __name__ == '__main__':
+    road = Road(5000, 20)
+    print(f'Для изготовления покрытия дороги нужно {road.calculate()} тонн.')
